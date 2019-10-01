dae08462f5ddad5ed3b52e9e3efcd1dcbbf7cdc6
kawashima41 <i.kawashima41@gmail.com>
2019-10-01 20:54:26 +0900

    Initial Commit

---
+++ YouTuberBusters/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YouTuberBusters
+//
+//  Created by Iichiro Kawashima on 2019/10/01.
+//  Copyright © 2019 Iichiro Kawashima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
